cfe90fd849b36710dec9b44919f3c12733e62bdf
MrWaffles545 <abarrwill@gmail.com>
2022-12-08 09:30:24 -0800

    stuff

--- JustDandy/Assets/Scripts/BeetleController.cs
+++ JustDandy/Assets/Scripts/BeetleController.cs
@@ -91,12 +91,4 @@ public class BeetleController : MonoBehaviour
             gameObject.transform.rotation = Quaternion.identity;
         }
     }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Attack")
-        {
-            health--;
-        }
-    }
 }
