3d280332d6c98a89ea3028fe35737985255237b6
Person1246811 <118207237+Person1246811@users.noreply.github.com>
2022-12-09 08:07:01 -0800

    Slash sorta fix

--- JustDandy/Assets/Scripts/Environment.cs
+++ JustDandy/Assets/Scripts/Environment.cs
@@ -18,7 +18,7 @@ public class Environment : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Attack")
+        if(collision.gameObject.tag == "Attack" && collision.gameObject.name != "Slash")
         {
             Destroy(collision.gameObject);
         }
